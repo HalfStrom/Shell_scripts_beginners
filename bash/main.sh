9af72aadd1e91fa810864a27bb9330d623bfbb18
HalfStrom <halfstorm42@gmail.com>
2025-05-04 20:39:38 -0300

    Add more news scripts codes in Bash/Shell script

---
+++ bash/main.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# main.sh
+. ./permission.lib
+echo $STD_MSG
+execute .sh
--- bash/variables3.sh
+++ bash/variables3.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo -en "What's your name [ `whoami` ] "
 read myname
-if [ -z $myname ]; then
-	myname=`whoami`
-fi
-echo "Your name is : $myname"
+#if [ -z $myname ]; then
+#	myname=`whoami`
+#fi
+echo "Your name is : ${myname:-`whoami`}"
